6ab5d0b23767496fc526e91733350d219cfb05bb
nelchacon <nelchacon@gmail.com>
2020-12-18 17:09:03 -0400

    Data upload

--- Code own project Nelson Chacon.R
+++ Code own project Nelson Chacon.R
@@ -2,19 +2,6 @@
 #Own Project Script: Predicting labor informality
 ##############################
 
-#Load database from github repository
-#if(!require(haven)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-
-#library(haven)
-#BOL_2018m11_BID <- read_dta("BOL_2018m11_BID.dta")
-#View(BOL_2018m11_BID)
-
-#Save data
-#save.image("es_2018.Rdata")
-
-#load data
-#load(file = "es_2018.Rdata")
-
 #Install required packages
 if(!require(foreign)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
@@ -22,8 +9,11 @@ if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.
 #if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 #if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-if(!require(gt)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(gt)) install.packages("gt", repos = "http://cran.us.r-project.org")
 #if(!require(scales)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(repmis)) install.packages("repmis", repos = "http://cran.us.r-project.org")
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+
 
 #Load libraries we will use
 library(tidyverse)
@@ -34,3 +24,14 @@ library(ggplot2)
 library(gt)
 library(foreign)
 #library(scales)
+library(repmis)
+library (readr)
+
+
+#Load database from github repository (csv format)
+
+urlfile="https://raw.githubusercontent.com/nelchacon/My_project_edx/main/hs_2018.csv"
+
+mydata<-read_csv(url(urlfile))
+
+
