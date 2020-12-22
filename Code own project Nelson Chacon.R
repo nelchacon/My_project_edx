567759a4f497fa63772563254a203daa0eb363f0
nelchacon <nelchacon@gmail.com>
2020-12-21 21:53:04 -0400

    imputation

--- Code own project Nelson Chacon.R
+++ Code own project Nelson Chacon.R
@@ -153,7 +153,104 @@ ggplot(data, aes(fill=categories, y=value, x=categories)) +
 
 # Pre-processing
 
+#Working only with economically active population
+
+table(mydata$pea_ci) #We have 17659 EAP in our sample
+
+eap.data <- filter(mydata, pea_ci==1)
+View(eap.data)
+
+#Cleaning outliers
+
+boxplot(eap.data$edad_ci)  #there are some outliers
+boxplot(eap.data$horaspri_ci) #there are some outliers
+boxplot(eap.data$ylmpri_ci) #there are many outliers
+boxplot(eap.data$aedu_ci) # no outliers
+boxplot(eap.data$antiguedad_ci) #there are many outliers too
+
+#creating Tukey's rule for removing outliers
+max_age <- quantile(eap.data$edad_ci, 0.75) + 1.5*IQR(eap.data$edad_ci)
+max_age  #the maximum age for avoiding outlier values is 83 years
+
+max_hours <- quantile(eap.data$horaspri_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$horaspri_ci, na.rm=T)
+max_hours  #the maximum number of working hours per week for avoiding outliers is 90 hours
+
+max_income <- quantile(eap.data$ylmpri_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$ylmpri_ci, na.rm=T)
+max_income  #the maximum labor income for avoiding outliers is 7750 Bolivianos (around 1.100 US$)
+
+max_senior <- quantile(eap.data$antiguedad_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$antiguedad_ci, na.rm=T)
+max_senior  #the maximum labor seniority for avoiding outliers is 28 years
+
+#Lets clean the data from these extreme values
+
+eap.data.no <- filter(eap.data, edad_ci <= 83, horaspri_ci <= 90, ylmpri_ci <=7750, antiguedad_ci <=28)
+View(eap.data.no) #we deleted 2878 extreme observations from our data base
+
+#Imputing missing values
+
+#first lets define all non numeric variables as factors
+str(eap.data.no)
+eap.data.no$zona_c <- as.factor(eap.data.no$zona_c)
+eap.data.no$sexo_ci <- as.factor(eap.data.no$sexo_ci)
+eap.data.no$id_ind_ci <- as.factor(eap.data.no$id_ind_ci)
+eap.data.no$categopri_ci <- as.factor(eap.data.no$categopri_ci)
+eap.data.no$rama_ci <- as.factor(eap.data.no$rama_ci)
+eap.data.no$sexo_ci <- as.factor(eap.data.no$sexo_ci)
+eap.data.no$tamemp_ci <- as.factor(eap.data.no$tamemp_ci)
+eap.data.no$mt <- as.factor(eap.data.no$mt)
+eap.data.no$p0 <- as.factor(eap.data.no$p0)
+eap.data.no$nempleos_ci <- as.factor(eap.data.no$nempleos_ci)
+eap.data.no$viviprop_ch <- as.factor(eap.data.no$viviprop_ch)
+eap.data.no$cobersal <- as.factor(eap.data.no$cobersal)
+
+str(eap.data.no)  #now the variables are numeric or factors
+
+#Now lets track which variables have missing data
+summary(eap.data.no)
+sum(is.na(eap.data.no))  # we have 562 missing data observations /(firm size, education and poverty)
+
+eap.data.no$missFirm <- ifelse(is.na(eap.data.no$tamemp_ci), "Y", "N") #want to know where are the NAs
+eap.data.no$misseduc <- ifelse(is.na(eap.data.no$aedu_ci), "Y", "N") 
+eap.data.no$missPov <- ifelse(is.na(eap.data.no$p0), "Y", "N") 
+
+#we erase the variables that not help for the imputation
+
+forimputation <- select(eap.data.no, -6, -7, -11, -21:-23)
+View(forimputation)
+str(forimputation)
+
+#transforming all variables to dummies
+
+dummy.vars <- dummyVars(~., data=forimputation)
+train.dummy <- predict(dummy.vars, forimputation)
+View(train.dummy)
+
+#now impute
+
+pre.process <- preProcess(train.dummy, method ="bagImpute")
+imputed.data <- predict(pre.process, train.dummy)
+View(imputed.data)
+
+
+
+
+
+
+
+
+#Creating the final database for our models
+
+#Split data into training and testing sets
+
+
+
+
 #Running Model 1: CART
+
+model1 <- train(formal_ci ~ .,
+                method = "rpart",
+                tuneGrid = data.frame(cp= seq(0, 0.05, len = 25)),
+                data = mydata)
 
 
 #Running Model 2: Random Forest
