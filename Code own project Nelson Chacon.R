b7d8adca85e4a8e6417fe514df310c49cee22514
nelchacon <nelchacon@gmail.com>
2020-12-22 11:42:10 -0400

    renaming

--- Code own project Nelson Chacon.R
+++ Code own project Nelson Chacon.R
@@ -39,58 +39,58 @@ View(mydata)
 summary(mydata)
 
 # Percentage of informal workers by gender
-table1 <- table(mydata$sexo_ci, mydata$formal_ci)
+table1 <- table(mydata$gender, mydata$formality)
 table1
 prop.table(table1,1)
 
 
 # Percentage of informal workers by zone
-table2 <- table(mydata$zona_c, mydata$formal_ci)
+table2 <- table(mydata$zone, mydata$formality)
 table2
 prop.table(table2,1)
 
 # Percentage of informal workers by race
-table3 <- table(mydata$id_ind_ci, mydata$formal_ci)
+table3 <- table(mydata$indigenous, mydata$formality)
 table3
 prop.table(table3,1)
 
 
 # Percentage of informal workers by labor category
-table4 <- table(mydata$categopri_ci, mydata$formal_ci)
+table4 <- table(mydata$labcat, mydata$formality)
 table4
 prop.table(table4,1)
 
 
 # Percentage of informal workers by labor branch
-table5 <- table(mydata$rama_ci, mydata$formal_ci)
+table5 <- table(mydata$labbranch, mydata$formality)
 table5
 prop.table(table5,1)
 
 # Percentage of informal workers by firm size
-table6 <- table(mydata$tamemp_ci, mydata$formal_ci)
+table6 <- table(mydata$firm_size, mydata$formality)
 table6
 prop.table(table6,1)
 
 
 # Percentage of informal workers by labor market
-table7 <- table(mydata$mt, mydata$formal_ci)
+table7 <- table(mydata$labor_market, mydata$formality)
 table7
 prop.table(table7,1)
 
 # Percentage of informal workers by labor market
-table8 <- table(mydata$p0, mydata$formal_ci)
+table8 <- table(mydata$poverty, mydata$formality)
 table8
 prop.table(table8,1)
 
 #average income for formal and informal workers
-mydata %>% group_by(formal_ci) %>% summarize(Avg = mean(ylmpri_ci, na.rm=T))
+mydata %>% group_by(formality) %>% summarize(Avg = mean(work_income, na.rm=T))
 
 
 #average working hours for formal and informal workers
-mydata %>% group_by(formal_ci) %>% summarize(Avg = mean(horaspri_ci, na.rm=T))
+mydata %>% group_by(formality) %>% summarize(Avg = mean(work_hours, na.rm=T))
 
 #average labor seniority for formal and informal workers
-mydata %>% group_by(formal_ci) %>% summarize(Avg = mean(antiguedad_ci, na.rm=T))
+mydata %>% group_by(formality) %>% summarize(Avg = mean(job_tenure, na.rm=T))
 
 #Graphics
 
@@ -153,55 +153,54 @@ ggplot(data, aes(fill=categories, y=value, x=categories)) +
 
 # Pre-processing
 
-#Working only with economically active population
+#Working only with economically active population (EAP)
 
-table(mydata$pea_ci) #We have 17659 EAP in our sample
+table(mydata$eapop) #We have 17659 EAP in our sample
 
-eap.data <- filter(mydata, pea_ci==1)
+eap.data <- filter(mydata, eapop==1)
 View(eap.data)
 
 #Cleaning outliers
 
-boxplot(eap.data$edad_ci)  #there are some outliers
-boxplot(eap.data$horaspri_ci) #there are some outliers
-boxplot(eap.data$ylmpri_ci) #there are many outliers
-boxplot(eap.data$aedu_ci) # no outliers
-boxplot(eap.data$antiguedad_ci) #there are many outliers too
+boxplot(eap.data$age)  #there are some outliers
+boxplot(eap.data$work_hours) #there are some outliers
+boxplot(eap.data$work_income) #there are many outliers
+boxplot(eap.data$educ) # no outliers
+boxplot(eap.data$job_tenure) #there are many outliers too
 
 #creating Tukey's rule for removing outliers
-max_age <- quantile(eap.data$edad_ci, 0.75) + 1.5*IQR(eap.data$edad_ci)
+max_age <- quantile(eap.data$age, 0.75) + 1.5*IQR(eap.data$age)
 max_age  #the maximum age for avoiding outlier values is 83 years
 
-max_hours <- quantile(eap.data$horaspri_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$horaspri_ci, na.rm=T)
+max_hours <- quantile(eap.data$work_hours, 0.75, na.rm=T) + 1.5*IQR(eap.data$work_hours, na.rm=T)
 max_hours  #the maximum number of working hours per week for avoiding outliers is 90 hours
 
-max_income <- quantile(eap.data$ylmpri_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$ylmpri_ci, na.rm=T)
+max_income <- quantile(eap.data$work_income, 0.75, na.rm=T) + 1.5*IQR(eap.data$work_income, na.rm=T)
 max_income  #the maximum labor income for avoiding outliers is 7750 Bolivianos (around 1.100 US$)
 
-max_senior <- quantile(eap.data$antiguedad_ci, 0.75, na.rm=T) + 1.5*IQR(eap.data$antiguedad_ci, na.rm=T)
+max_senior <- quantile(eap.data$job_tenure, 0.75, na.rm=T) + 1.5*IQR(eap.data$job_tenure, na.rm=T)
 max_senior  #the maximum labor seniority for avoiding outliers is 28 years
 
 #Lets clean the data from these extreme values
 
-eap.data.no <- filter(eap.data, edad_ci <= 83, horaspri_ci <= 90, ylmpri_ci <=7750, antiguedad_ci <=28)
+eap.data.no <- filter(eap.data, age <= 83, work_hours <= 90, work_income <=7750, job_tenure <=28)
 View(eap.data.no) #we deleted 2878 extreme observations from our data base
 
 #Imputing missing values
 
 #first lets define all non numeric variables as factors
 str(eap.data.no)
-eap.data.no$zona_c <- as.factor(eap.data.no$zona_c)
-eap.data.no$sexo_ci <- as.factor(eap.data.no$sexo_ci)
-eap.data.no$id_ind_ci <- as.factor(eap.data.no$id_ind_ci)
-eap.data.no$categopri_ci <- as.factor(eap.data.no$categopri_ci)
-eap.data.no$rama_ci <- as.factor(eap.data.no$rama_ci)
-eap.data.no$sexo_ci <- as.factor(eap.data.no$sexo_ci)
-eap.data.no$tamemp_ci <- as.factor(eap.data.no$tamemp_ci)
-eap.data.no$mt <- as.factor(eap.data.no$mt)
-eap.data.no$p0 <- as.factor(eap.data.no$p0)
-eap.data.no$nempleos_ci <- as.factor(eap.data.no$nempleos_ci)
-eap.data.no$viviprop_ch <- as.factor(eap.data.no$viviprop_ch)
-eap.data.no$cobersal <- as.factor(eap.data.no$cobersal)
+eap.data.no$zone <- as.factor(eap.data.no$zone)
+eap.data.no$gender <- as.factor(eap.data.no$gender)
+eap.data.no$indigenous <- as.factor(eap.data.no$indigenous)
+eap.data.no$labcat <- as.factor(eap.data.no$labcat)
+eap.data.no$labbranch <- as.factor(eap.data.no$labbranch)
+eap.data.no$firm_size <- as.factor(eap.data.no$firm_size)
+eap.data.no$labor_market <- as.factor(eap.data.no$labor_market)
+eap.data.no$poverty <- as.factor(eap.data.no$poverty)
+eap.data.no$number_jobs <- as.factor(eap.data.no$number_jobs)
+eap.data.no$house_prop <- as.factor(eap.data.no$house_prop)
+eap.data.no$health_insure <- as.factor(eap.data.no$health_insure)
 
 str(eap.data.no)  #now the variables are numeric or factors
 
@@ -209,13 +208,13 @@ str(eap.data.no)  #now the variables are numeric or factors
 summary(eap.data.no)
 sum(is.na(eap.data.no))  # we have 562 missing data observations /(firm size, education and poverty)
 
-eap.data.no$missFirm <- ifelse(is.na(eap.data.no$tamemp_ci), "Y", "N") #want to know where are the NAs
-eap.data.no$misseduc <- ifelse(is.na(eap.data.no$aedu_ci), "Y", "N") 
-eap.data.no$missPov <- ifelse(is.na(eap.data.no$p0), "Y", "N") 
+eap.data.no$missFirm <- ifelse(is.na(eap.data.no$firm_size), "Y", "N") #want to know where are the NAs
+eap.data.no$misseduc <- ifelse(is.na(eap.data.no$educ), "Y", "N") 
+eap.data.no$missPov <- ifelse(is.na(eap.data.no$poverty), "Y", "N") 
 
 #we erase the variables that not help for the imputation
 
-forimputation <- select(eap.data.no, -6, -7, -11, -21:-23)
+forimputation <- select(eap.data.no, -6, -10, -20:-22)
 View(forimputation)
 str(forimputation)
 
